d747d1732c6ca7b2995503acb352118f8bdb6d5a
Roddy <roddy of the frozen pas>
2014-09-07 17:15:06 -0600

    Utility class to min supported version for features
    Certain features were enabled in certain versions, and further tweaked
    in later versions. In order to keep track of these, this utility class
    'knows' the minimum release version of each feature and any subsequent
    serious changes. This way, instead of cluttering our files with checks
    against raw numbers that may get confused or mistyped, we can just query
    the utility class.

---
+++ TEdit/Data/Util/VersionUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEdit.Data.Util
+{
+    class VersionUtils
+    {
+        #region Properties
+        
+        private static const int MIN_RELEASE_DIFFICULTY = 10;
+        private static const int MIN_RELEASE_DIFFICULTY_AS_BYTE = 17;
+
+        private static const int MIN_RELEASE_HAIR_DYE = 82;
+
+        private static const int MIN_RELEASE_HIDE_VISUAL = 83;
+
+        private static const int MIN_RELEASE_BINARY_GENDER = 17;
+
+        #endregion
+
+        public static bool IsDifficultySupported(int release)
+        {
+            return release >= MIN_RELEASE_DIFFICULTY;
+        }
+
+        public static bool IsDifficultyAByte(int release)
+        {
+            return release >= MIN_RELEASE_DIFFICULTY_AS_BYTE;
+        }
+
+        public static bool IsHairDyeSupported(int release)
+        {
+            return release >= MIN_RELEASE_HAIR_DYE;
+        }
+
+        public static bool IsHideVisualSupported(int release)
+        {
+            return release >= MIN_RELEASE_HIDE_VISUAL;
+        }
+
+        public static Boolean IsBinaryGenderSupported(int release)
+        {
+            return release >= MIN_RELEASE_BINARY_GENDER;
+        }
+    }
+}
