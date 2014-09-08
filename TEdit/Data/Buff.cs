1894495d810a5ca150bfefe375b3b736cc08d224
Roddy <roddy of the frozen pas>
2014-09-07 21:24:56 -0600

    Buffer class
    To model buffs. Not directly used by TEdit (yet) but needed for saving.

---
+++ TEdit/Data/Buff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEdit.Data
+{
+    class Buff
+    {
+        public Int32 Type { get; set; }
+        public Int32 Time { get; set; }
+    }
+}
