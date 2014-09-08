eebd59fbddad92f7d808c57401bed506c5a21cc8
Roddy <roddy of the frozen pas>
2014-09-07 21:26:59 -0600

    Fix data types in Item

--- TEdit/Data/Item.cs
+++ TEdit/Data/Item.cs
@@ -8,19 +8,21 @@ namespace TEdit.Data
 {
     /// <summary>
     /// Models an in-game Item. At this point it's only going to hold the properties we need
-    /// for the Player editor though we can conceivably implement an Item editor. (TODO)
+    /// for the Player editor, though we can conceivably implement an Item editor. (TODO)
     /// </summary>
     class Item
     {
         public Int32 StackSize { get; set; }
-        public short Id { get; set; }
-        public short Prefix { get; set; }
+        public Int32 Id { get; set; }
+        public int Prefix { get; set; }
+        public string Name { get; set; }
 
         public Item()
         {
             Id = 0; // "No item"
             StackSize = 0;
             Prefix = 0;
+            Name = "";
         }
     }
 }
