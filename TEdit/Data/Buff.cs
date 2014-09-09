f02b41a86231025e2f8a3d6698971de2cc251e77
Roddy <roddyofthefrozenpeas@gmail.com>
2014-09-08 22:14:46 -0600

    Default values and override Equals() in Buff, Item

--- TEdit/Data/Buff.cs
+++ TEdit/Data/Buff.cs
@@ -8,7 +8,39 @@ namespace TEdit.Data
 {
     class Buff
     {
+        [System.ComponentModel.DefaultValue(0)]
         public Int32 Type { get; set; }
+
+        [System.ComponentModel.DefaultValue(0)]
         public Int32 Time { get; set; }
+        
+        public override bool Equals(System.Object obj)
+        {
+            Buff b = obj as Buff;
+            if ((object)b == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return Equals(b);
+        }
+
+        public bool Equals(Buff b)
+        {
+
+            if (b == null)
+            {
+                return false;
+            }
+
+            return Type.Equals(b.Type) &&
+                   Time.Equals(b.Time);
+        }
+
+        public override int GetHashCode()
+        {
+            return Type.GetHashCode() ^ Time.GetHashCode();
+        }
     }
 }
--- TEdit/Data/Item.cs
+++ TEdit/Data/Item.cs
@@ -12,17 +12,58 @@ namespace TEdit.Data
     /// </summary>
     class Item
     {
+        [System.ComponentModel.DefaultValue(0)]
         public Int32 StackSize { get; set; }
-        public Int32 Id { get; set; }
-        public int Prefix { get; set; }
-        public string Name { get; set; }
 
-        public Item()
+        [System.ComponentModel.DefaultValue(0)]
+        public Int32 Id { get; set; }
+
+        [System.ComponentModel.DefaultValue(0)]
+        public int Prefix { get; set; }
+
+        [System.ComponentModel.DefaultValue("")]
+        public string Name { get; set; }
+        
+        public override string ToString()
         {
-            Id = 0; // "No item"
-            StackSize = 0;
-            Prefix = 0;
-            Name = "";
+            return string.Format("{0} {1} ({2}) x{3}", Prefix, Name, Id, StackSize);
+        }
+
+        public override bool Equals(System.Object obj)
+        {
+            Buff b = obj as Buff;
+            if ((object)b == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return Equals(b);
+        }
+
+        public bool Equals(Item i)
+        {
+
+            if (i == null)
+            {
+                return false;
+            }
+
+            return StackSize.Equals(i.StackSize) &&
+                   Id.Equals(i.Id) &&
+                   IsBothNullOrEqual(Name, i.Name) &&
+                   Prefix == i.Prefix;
+        }
+        
+        private static bool IsBothNullOrEqual(string a, string b)
+        {
+            return (a == null && b == null) ||
+                (a != null && b != null && a.Equals(b));
+        }
+
+        public override int GetHashCode()
+        {
+            return StackSize.GetHashCode() ^ Id.GetHashCode() ^ Name.GetHashCode() ^ Prefix.GetHashCode();
         }
     }
 }
